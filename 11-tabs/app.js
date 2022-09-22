e8d667b4eb43b73e4816e28b762354d09831e339
ltdat26 <ltdat26@gmail.com>
2022-09-22 17:46:42 +0700

    commit

---
+++ 11-tabs/app.js
@@ -0,0 +1,17 @@
+const about = document.querySelector(".about")
+const btns = document.querySelectorAll(".tab-btn")
+const articles = document.querySelectorAll(".content")
+about.addEventListener("click", (e) => {
+  const id = e.target.dataset.id
+  if (id) {
+    btns.forEach((btn) => {
+      btn.classList.remove("active")
+    })
+    e.target.classList.add("active")
+    articles.forEach((article) => {
+      article.classList.remove("active")
+    })
+    const el = document.getElementById(id)
+    el.classList.add("active")
+  }
+})
